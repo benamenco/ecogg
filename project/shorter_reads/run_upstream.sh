fd132d8baf9c9777ce6c73fed244241078c3f74a
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-04-14 13:36:58 +0200

    rm duplicate files

--- project/shorter_reads/run_upstream.sh
+++ project/shorter_reads/run_upstream.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+require_program fas_proportionally_shorten.rb
+
 if [ ! -e filtered.fna ]; then
   echo "Filter reads"
   ./filter_reads.rb allsamples.fna "(L4|H2.2)" > filtered.fna
@@ -8,7 +11,7 @@ fi
 for factor in 0.125 0.250 0.500; do
   if [ ! -e shortened.$factor.fna ]; then
     echo "Shorten with factor $factor"
-    ./proportionally_shorten_fasta.rb filtered.fna $factor \
+    fas_shorten_proportionally.rb filtered.fna $factor \
       > shortened.$factor.fna &
   fi
 done
