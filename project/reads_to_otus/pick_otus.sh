47b56b92d17671d764e946410b1564e3e5c0c5d4
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-04-14 11:22:53 +0200

    better document pick_otus procedure

--- project/reads_to_otus/pick_otus.sh
+++ project/reads_to_otus/pick_otus.sh
@@ -7,6 +7,7 @@ require_var silva $silva "Path to Silva database"
 require_var NSLOTS $NSLOTS "Number of cluster slots to use"
 
 require_program pick_open_reference_otus.py
+require_program filter_otus_from_otu_table.py
 require_program uclust
 require_program FastTree
 require_program pynast
@@ -16,4 +17,13 @@ require_file $silva
 
 echo -e “pick_otus:enable_rev_strand_match\tTrue” > pick_otus.params
 pick_open_reference_otus.py -i $allsamplesfna -o $outdir -f \
-    -m uclust -r $silva -a -O $NSLOTS -p pick_otus.params --min_otu_size 2
+    -m uclust -r $silva -a -O $NSLOTS -p pick_otus.params --min_otu_size 1
+
+all=$outdir/otu_table_mc1_w_tax.biom
+require_file $all
+
+nonsingletons=$outdir/otu_table_mc2_w_tax.biom
+filter_otus_from_otu_table.py -v -n 2 -i $all -o $nonsingletons
+
+# this is the output file which we use for subsequent analyses:
+require_file $nonsingletons
