70a535d2bd1b2d29330fe1c079a3862bce48482a
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2016-04-15 10:34:02 +0200

    rm some unused scripts

--- project/cmp_abundances/plot_data.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-library("ggplot2")
-args<-commandArgs(trailingOnly=T)
-fn=paste0(args[1],".dat")
-t<-read.table(fn)
-p<-ggplot(t,aes(V1,V2))+
-   xlab("Vents abundance")+ylab("Open Ocean abundance")
-p+geom_point()
